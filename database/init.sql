ac2fd656024859887b1af076575ed99038b23d54
Alice Karlén <alice.karlen@hotmail.com>
2026-09-15 13:33:17 +0200

    feat: initialize Docker setup with Dockerfiles and docker-compose for frontend and backend

---
+++ database/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS users (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(256) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY users_username_unique (username)
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+  userId INT(11) UNSIGNED NOT NULL,
+  token VARCHAR(6) UNSIGNED NOT NULL,
+  UNIQUE KEY sessions_token_unique (token)
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  userId INT(11) UNSIGNED NOT NULL,
+  amount INT(20) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY accounts_id_unique (id)
+);
